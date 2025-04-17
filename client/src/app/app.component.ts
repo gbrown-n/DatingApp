c5ffb636d19fc64962732d9cec94d8eeb519bca3
gabrielbrown1 <gabriel.brown@valuepro.com.au>
2025-04-17 13:49:40 +1000

    Section 3 - Angular Skeleton

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  http=inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/user').subscribe({
+      next: response => this.users=response, 
+      error: error => console.log(error), 
+      complete: () => console.log('Request completed')
+    })
+  }
+}
